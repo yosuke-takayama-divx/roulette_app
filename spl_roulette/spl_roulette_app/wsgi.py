6fb5320271dd6be4405df821d449e1756994248a
yosuke.takayama.ex <yosuke.takayama@paygent.co.jp>
2024-08-24 13:58:27 +0900

    修正

--- spl_roulette/spl_roulette_app/wsgi.py
+++ spl_roulette/spl_roulette_app/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spl_roulette_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spl_roulette.spl_roulette_app.settings")
 
 application = get_wsgi_application()
