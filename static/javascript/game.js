2cb3a1740c5c7a3d712f4b20e3deeee639fae6af
yosuke.takayama.ex <yosuke.takayama@paygent.co.jp>
2024-08-24 18:01:13 +0900

    html,js修正

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -9,25 +9,17 @@ socket.onopen = function() {
 
 socket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-
-    // data.indexがundefinedでないことを確認
     if (data.index !== undefined) {
         const userNameInput = document.getElementById("userName" + data.index);
-
         if (userNameInput) {
             const userName = userNameInput.value || "あなた";
-
-            // 結果を保存
             results[data.index] = {
                 userName: userName,
                 imageName: data.image_name,
                 imagePath: data.image_path
             };
-
-            // 選ばれた武器一覧を更新
             const listResultName = document.getElementById("list_resultName" + data.index);
             listResultName.innerText = userName + 'の武器は ' + data.image_name + ' です！';
-
             // 現在のインデックスを更新
             if (currentIndex === data.index) {
                 updateResultDisplay(data.index);
@@ -48,12 +40,8 @@ socket.onclose = function() {
     console.log("WebSocket接続が閉じられました。");
 };
 
-// スピンボタン
 function spinRoulette(event) {
-    // ページリロード防止
     event.preventDefault();
-
-    // 各入力フィールドの値を取得して送信
     for (let i = 1; i <= 10; i++) {
         const userNameInput = document.getElementById("userName" + i);
         const userName = userNameInput ? userNameInput.value || "あなた" : "あなた";
@@ -61,7 +49,6 @@ function spinRoulette(event) {
     }
 }
 
-// 結果表示ボタン
 function showResult(index) {
     const result = results[index];
     const errorMessage = document.getElementById("errorMessage");
@@ -74,7 +61,6 @@ function showResult(index) {
     }
 }
 
-// 結果表示を更新する関数
 function updateResultDisplay(index) {
     const result = results[index];
     const img = document.getElementById("resultImage");
@@ -83,16 +69,13 @@ function updateResultDisplay(index) {
     name.innerText = result.userName + 'の武器は ' + result.imageName + ' です！';
 }
 
-// ページがロードされた時0.jpgを表示
 document.addEventListener("DOMContentLoaded", function() {
     const img = document.getElementById("resultImage");
     img.src = STATIC_URL + "images/0.jpg";
+    document.getElementById("spinButton").onclick = spinRoulette;  // スピンボタンのIDを直接こちらに書きました
 });
 
 document.getElementById("btn_form_wrap").onclick = function() {
     const formWrap = document.getElementById("form_wrap");
     formWrap.classList.toggle("toggle_open");
 };
-
-// スピンボタンのクリックハンドラーを設定
-document.getElementById("spinButton").onclick = spinRoulette;  // スピンボタンのIDを確認
